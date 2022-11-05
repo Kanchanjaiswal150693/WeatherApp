7f0ef30f0cd69d9eec591d651e4e3f189b0672f6
Kanchan Jaiswal <117472206+Kanchanjaiswal150693@users.noreply.github.com>
2022-11-05 18:57:12 +0200

    Update App.js
    Deleted icon and search import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useState } from 'react';
-import { Icon } from 'react-icons-kit'
-import { search } from 'react-icons-kit/feather/search'
+//import { Icon } from 'react-icons-kit'
+//import { search } from 'react-icons-kit/feather/search'
 import { WeatherViewer } from './Components/WeatherViewer'
 
 function App() {
